4fabe52c29d2c68fe493945c6ac1ee747781fcf3
Elizabeth Hernandez <eeelizabeth@Elizabeths-MacBook-Air.local>
2020-02-15 11:13:10 -0800

    First commit

---
+++ Resplandecer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Resplandecer
+//
+//  Created by Elizabeth Hernandez on 2/15/20.
+//  Copyright © 2020 Resplandecer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Resplandecer!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
