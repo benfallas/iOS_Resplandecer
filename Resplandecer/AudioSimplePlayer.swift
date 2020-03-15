75cf6941ee4ae0a18642a1d4b659b29c7d3a191f
juhipark <juhipark@student.hartnell.edu>
2020-03-15 13:40:02 -0700

    Created AudioSimplePlayer

---
+++ Resplandecer/AudioSimplePlayer.swift
@@ -0,0 +1,11 @@
+//
+//  AudioSimplePlayer.swift
+//  Resplandecer
+//
+//  Created by Juhi on 2/27/20.
+//  Copyright © 2020 Resplandecer. All rights reserved.
+//
+
+import Foundation
+
+
--- Resplandecer/MenuView.swift
+++ Resplandecer/MenuView.swift
@@ -1,5 +1,5 @@
 //
-//  sideMenu.swift
+//  MenuContent.swift
 //  myPart
 //
 //  Created by Elizabeth Hernandez on 11/12/19.
