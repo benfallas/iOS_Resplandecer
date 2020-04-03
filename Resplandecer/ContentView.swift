0b35bf3cba7042b0bd636aa15da3ea766e9a66dc
juhipark <juhipark@student.hartnell.edu>
2020-04-02 18:03:51 -0700

    Player member methods added

--- Resplandecer/ContentView.swift
+++ Resplandecer/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
 
                     Text("Hello, Resplandecer!")
                 }
-                SideMenu(width: 270,
+                SideMenu(width: 270,g
                          isOpen: self.menuOpen,
                          menuClose: self.openMenu)
                          
